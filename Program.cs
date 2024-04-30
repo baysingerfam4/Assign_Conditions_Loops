a8288b22ee290610288aa0716e761b07b858292c
baysingerfam4 <165937206+baysingerfam4@users.noreply.github.com>
2024-04-30 00:20:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Assign_Conditions_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1();
+        }
+
+        public static void Problem1()
+        {
+            Console.WriteLine("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            bool positive = num > 0;
+            bool negative = num < 0;
+            bool zero = num == 0;
+            if (positive)
+            {
+                Console.WriteLine("positive");
+            }
+
+            else if (negative)
+            {
+                Console.WriteLine("negative");
+            }
+            else if (zero)
+            {
+                Console.WriteLine("zero");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+            
+        }
+    } //class
+} //namespace
